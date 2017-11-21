6387fd3c2399117d43784209b117f550a1f0ba81
Adhi Ravishankar <adhiravishankar@gmail.com>
2017-11-21 14:05:13 -0500

    initial commit 2

---
+++ app/src/main/java/com/adhiravishankar/dronecomputervision/FrescoActivity.java
@@ -0,0 +1,13 @@
+package com.adhiravishankar.dronecomputervision;
+
+import android.app.Activity;
+
+/**
+ * Created by adhi on 11/20/17.
+ */
+
+public class FrescoActivity extends Activity {
+
+
+
+}
---
+++ app/src/main/java/com/adhiravishankar/dronecomputervision/MainActivity.java
@@ -0,0 +1,39 @@
+package com.adhiravishankar.dronecomputervision;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by adhi on 11/2/17.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        } else {
+            startActivity(new Intent(this, FdActivity.class));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(this, FdActivity.class));
+            }
+        }
+    }
+
+}
